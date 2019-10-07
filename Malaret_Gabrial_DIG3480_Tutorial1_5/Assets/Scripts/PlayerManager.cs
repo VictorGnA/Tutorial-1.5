49d0071b97f14703b1dc5f4c21ccc9049e3ca590
Gabe Malaret <44530082+VictorGnA@users.noreply.github.com>
2019-10-07 00:06:53 -0400

    Tutorial script

---
+++ Malaret_Gabrial_DIG3480_Tutorial1_5/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp("KeyCode.D"))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp("KeyCode.F"))
+        {
+            anim.SetInteger("State", 0);
+        }
+    }
+
+
+
+
+}
